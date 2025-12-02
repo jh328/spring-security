8aa73ec11c6003c3637d346c875dc69651ec2f65
jh328 <wnsgks32810@naver.com>
2025-12-02 16:04:32 +0900

    폼 로그인

--- src/main/java/io/security/springseuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springseuritymaster/SecurityConfig.java
@@ -24,12 +24,19 @@ import java.io.IOException;
 @Configuration
 public class SecurityConfig {
 
+    /**
+     * defaultSuccessUrl 옵션 alwaysUse 기본값은 false
+     * true 설정을 하게 되면 지정한 경로로 간다.
+     * true 아니면 경우에 따라서 처음에 시도한 경로로 간다.
+     * 인증을 받기 전에 어딘가 요청ㅇ르 했고,그 요청이 막혀서 인증을 성공을 하면
+     * 이전에 요청한 경로로 가게 되는거다.
+     * */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
                 .formLogin(form -> form
-                        .loginPage("/loginPage")
+//                        .loginPage("/loginPage")
                         .loginProcessingUrl("/loginProc")
                         .defaultSuccessUrl("/", true)
                         .failureUrl("/failed")
@@ -49,12 +56,24 @@ public class SecurityConfig {
                             System.out.println("authentication = " + authentication);
                             response.sendRedirect("/home");
                         })
-                        .failureHandler(new AuthenticationFailureHandler() {
+                        /*.failureHandler(new AuthenticationFailureHandler() {
                             @Override
                             public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-                                System.out.println();
+                                System.out.println("exception " + exception.getMessage());
+                                response.sendRedirect("/login");
                             }
+                        })*/
+                        /**
+                         * 예외가 발생 했을 경우 실패하게 되니깐 예외 정보가 넘어온다.
+                         * 실패 했을 경우 일반적으로 로그인 페이지로 이동하는게 일반적이다.
+                         * 로그인 페이지, url, faile 인증을 하지 못해도 접근이 가능해야 하기 때문에
+                         * .permitAll 옵션 설정을 한다.
+                         * */
+                        .failureHandler((request, response, exception) -> {
+                            System.out.println("exception " + exception.getMessage());
+                            response.sendRedirect("/login");
                         })
+                        .permitAll()
                 );
         return http.build();
     }
